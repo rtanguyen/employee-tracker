fa652fe87f679434adf095fb2f29e9b8cf5f7e48
rtanguyen <rtanguyen@gmail.com>
2021-06-08 16:59:57 -0500

    view functions

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 const initializePrompts = () => {
     inquirer.prompt([
     {
-        type: 'checkbox',
+        type: 'list',
         name: 'action',
         message: 'Please select from the following options:',
         choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit']
@@ -38,37 +38,80 @@ const initializePrompts = () => {
         case 'Update an employee role':
             updateEmployee();
             break;
-            
+        // case 'Quit':
+        //     endApp();
+        //     break;
+        default:
+            console.log("error");
         }
     });
 };
 
-
+//==================VIEW FUNCTIONS==================//
 const viewDepartments = () => {
-    const sql = `SELECT * FROM department`;
+    const sql = `SELECT name AS department_name FROM department`;
     
     db.query(sql, (err, rows) => {
         if (err) throw err;
         return console.table(rows);
     });
-    //TODO: add exit prompt?
+    initializePrompts();
 };
 
-function addDepartment() {
+const viewRoles = () => {
+    const sql = `SELECT role.id, role.title, role.salary, 
+                department.name AS department_name
+                FROM role
+                LEFT JOIN department
+                ON role.department_id = department.id`;
+    db.query(sql, (err, rows) => {
+        if (err) throw err;
+        return console.table(rows);
+    });
+    initializePrompts();
+};
+
+const viewEmployees = () => {
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name, 
+                role.title, role.salary, department.name, employee.manager_id,
+                CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+                FROM employee
+                LEFT JOIN employee manager ON manager.id = employee.manager_id
+                INNER JOIN role on employee.role_id = role.id
+                INNER JOIN department ON role.department_id = department.id`;
+    db.query(sql, (err, rows) => {
+        if (err) throw err;
+        return console.table(rows);
+    });
+    initializePrompts();
+};
+
+//==================ADD FUNCTIONS==================//
+const addDepartment = () => {
     inquirer.prompt(
         {
             type: 'input',
-            name: 'deptName',
+            name: 'name',
             message: 'Input name of department to add to database:',
-            validate: deptName => {
-                if (deptName) {
+            validate: name => {
+                if (name) {
                   return true;
                 } else {
                   console.log('Please enter department name');
                   return false
                 }
             }
-        });
+        })
+        .then((newDepartment) => {
+            const sql = `INSERT INTO department (name) VALUES (?)`;
+            const params = newDepartment;
+            console.log(params);
+            db.query(sql, params, (err, rows) => {
+                if (err) throw err;
+                console.log('New department successfully added');
+                initializePrompts();
+            });
+        })
 };
 
 initializePrompts();
